5f20e9a70c16a499325f2f4881483c5852c9573f
origami <origami1024@gmail.com>
2020-02-16 16:05:22 +0300

    fb validations

--- src/pgqueries.js
+++ src/pgqueries.js
@@ -1330,8 +1330,11 @@ async function feedback(req, res) {
     res.send('error, name too long')
     return false
   }
-  if (req.body.mail.length > 70) {
-    res.send('error, mail too long')
+  if (
+    req.body.mail.length > 70 ||
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(req.body.mail) == false
+  ) {
+    res.send('error, mail too bad')
     return false
   }
   if (req.body.topic.length > 2000) {
--- src/views/Feedback.vue
+++ src/views/Feedback.vue
@@ -2,27 +2,42 @@
   <div class="feedback">
     <div v-if="state == 'ready'" class="fb_inner">
       <br>
-      <q-input v-model="fbData.topic" square
-            dense
-            outlined
-            bg-color="white" color="cyan-10" label="Тема"/>
+      <q-input 
+        v-model="fbData.topic"
+        square
+        dense
+        outlined
+        bg-color="white" color="cyan-10" label="Тема"
+        counter
+        maxlength="25"
+      />
       <br>
-      <q-input square
-            dense
-            
-            bg-color="white" color="cyan-10" v-model="fbData.name" outlined label="Ваше имя"/>
+      <q-input
+        square
+        dense
+        counter
+        maxlength="25"    
+        bg-color="white" color="cyan-10" v-model="fbData.name" outlined label="Ваше имя"/>
       <br>
-      <q-input square
-            dense
-            bg-color="white" color="cyan-10" v-model="fbData.mail" outlined label="Email"/>
+      <q-input
+        square
+        dense
+        bg-color="white" color="cyan-10" v-model="fbData.mail" outlined label="* Email"
+        counter
+        maxlength="70"
+        :rules="[
+          val => !!val || 'Обязательное поле',
+          val => mailregex.test(val) || 'Некорректный формат адреса'
+        ]"
+      />        
       <br>
       <q-input 
         maxlength="500"
         counter
         square
-            dense
-            outlined
-            bg-color="white" color="cyan-10"
+        dense
+        outlined
+        bg-color="white" color="cyan-10"
         v-model="fbData.body"
         type="textarea"
         label="Пожелания"
@@ -45,13 +60,13 @@
 
 <script>
 import axios from 'axios'
-
 const config = require('./../configs/main_config')
 
 export default {
   name: 'FeedBack',
   data: ()=>{return {
     state: 'ready', //justSentOk, justSentBad
+    mailregex: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
     fbData: {
       topic: '',
       name: '',
